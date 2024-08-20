9a84e0c3ba3b13e187c225b92694192823b26098
muhammad Hamd <muhammad.hamd@cubitrek.com>
2024-08-20 15:01:17 +0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Run Gunicorn with Uvicorn workers
-exec gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:main_app --bind 0.0.0.0:8000
+exec gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:main_app
